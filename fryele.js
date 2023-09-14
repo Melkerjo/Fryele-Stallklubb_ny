1d7953b73a0622bf0bf9b6fc3aee4a8e6fb760f0
Melker Johansson <melker@newgen.se>
2023-09-14 15:48:27 +0200

    ny

--- fryele.js
+++ fryele.js
@@ -1,3 +1,20 @@
+
 var copyrightElement = document.getElementById("copyright");
-        var currentYear = new Date().getFullYear();
-        copyrightElement.textContent = "© " + currentYear + " Melker Johansson. Alla rättigheter förbehållna.";
+var currentYear = new Date().getFullYear();
+copyrightElement.textContent = "© " + currentYear + " Melker Johansson. Alla rättigheter förbehållna.";
+
+const menuIcon = document.querySelector('.menu-icon');
+const mobileMenu = document.querySelector('.mobileNav');
+
+menuIcon.addEventListener('click', () => {
+mobileMenu.classList.toggle('open');
+});
+
+// Stäng menyn om användaren klickar någonstans utanför menyn
+document.addEventListener('click', (event) => {
+if (!mobileMenu.contains(event.target) && !menuIcon.contains(event.target)) {
+mobileMenu.classList.remove('open');
+}
+});
+
+
